dfad734919c9c0b3d5c3cff3264f81ba0c1a3028
sdwru <admin@powerpbx.org>
2023-03-02 09:24:46 -0800

    * Updated to use Telnyx v2 API * Updated dialplan to use voicemail email address

--- telnyx.php
+++ telnyx.php
@@ -12,36 +12,39 @@ $logfile = '/tmp/sms.log'; // all SMSes and errors will be logged here for debug
 // Record the message
 $postdata = file_get_contents("php://input");
 $fh = fopen($logfile, "a");
-fwrite($fh, date(DATE_W3C) . " - Received SMS from Telnyx:\n");
 fwrite($fh, "$postdata\n\n");
 fclose($fh);
-
 // Respond to Telnyx
 echo 'OK' . "\n";
 
 // Find the recipient in astdb
-$sms = json_decode($postdata);
-if (preg_match("/\+1([2-9][0-9][0-9][2-9][0-9]{6})/", $sms->to , $matches)) {
-	$to = $matches[1];
-	$output = shell_exec('/usr/sbin/asterisk -rx "database showkey accountcode"');
-	$count = preg_match_all("#AMPUSER/([0-9]+)/accountcode.*: $to\s*$#m", $output, $exts);
-	if ($count) {
-		require_once('php-sip/PhpSIP.class.php');
-		$smsout = new PhpSIP('127.0.0.1', '5099');
-		foreach ($exts[1] as $ext) {
-			$smsout->newCall();
-			$smsout->setMethod('MESSAGE');
-			$smsout->setFrom('sip:' . $sms->from . '@127.0.0.1');
-			$smsout->setContentType('text/plain; charset=UTF-8');
-			$smsout->setBody($sms->{'body'});
-			$smsout->setUri('sip:' . $ext . '@127.0.0.1:' . $localUdpPort);
-			$res = $smsout->send(); 
+$sms = json_decode($postdata, true);
+if ($sms['data']['event_type'] != 'message.received') {
+	exit;
+}
+$payload = $sms['data']['payload'];
+foreach ($payload['to'] as $k=>$v) {
+	if (preg_match("/\+1([2-9][0-9][0-9][2-9][0-9]{6})/", $v['phone_number'] , $matches)) {
+		$to = $matches[1];
+		$output = shell_exec('/usr/sbin/asterisk -rx "database showkey accountcode"');
+		$count = preg_match_all("#AMPUSER/([0-9]+)/accountcode.*: $to\s*$#m", $output, $exts);
+		if ($count) {
+			require_once('php-sip/PhpSIP.class.php');
+			$smsout = new PhpSIP('127.0.0.1', '5099');
+			foreach ($exts[1] as $ext) {
+				$smsout->newCall();
+				$smsout->setMethod('MESSAGE');
+				$smsout->setFrom('sip:' . $payload['from']['phone_number'] . '@127.0.0.1');
+				$smsout->setContentType('text/plain; charset=UTF-8');
+				$smsout->setBody($payload['text']);
+				$smsout->setUri('sip:' . $ext . '@127.0.0.1:' . $localUdpPort);
+				$res = $smsout->send(); 
+			}
+		} else {
+			$fh = fopen($logfile, "a");
+			fwrite($fh, date($payload['received_at']) . " - Nowhere to send " . $payload['id'] . "\n\n");
+			fclose($fh);
 		}
-	} else {
-		$fh = fopen($logfile, "a");
-		fwrite($fh, date(DATE_W3C) . " - Nowhere to send " . $sms->sms_id . "\n\n");
-		fclose($fh);
 	}
 }
-
 ?>
